d98c337f406b197e5c9304b3254db905cbb42745
单开宇 <547913250@qq.com>
2021-08-21 15:36:05 +0800

    增加自定义权限验证接口扩展

---
+++ src/main/java/com/conifercone/uaa/service/impl/StpInterfaceImpl.java
@@ -0,0 +1,77 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) [2021] [sky5486560@gmail.com]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.conifercone.uaa.service.impl;
+
+import cn.dev33.satoken.stp.StpInterface;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 自定义权限验证接口扩展
+ *
+ * @author sky5486560@gmail.com
+ * @date 2021/8/21
+ */
+@Component
+public class StpInterfaceImpl implements StpInterface {
+
+    /**
+     * 获得权限列表
+     *
+     * @param loginId   登录id
+     * @param loginType 登录类型
+     * @return {@link List}<{@link String}>
+     */
+    @Override
+    public List<String> getPermissionList(Object loginId, String loginType) {
+        // 本list仅做模拟，实际项目中要根据具体业务逻辑来查询权限
+        List<String> list = new ArrayList<>();
+        list.add("101");
+        list.add("user-add");
+        list.add("user-delete");
+        list.add("user-update");
+        list.add("user-get");
+        list.add("article-get");
+        return list;
+    }
+
+    /**
+     * 获取角色列表
+     *
+     * @param loginId   登录id
+     * @param loginType 登录类型
+     * @return {@link List}<{@link String}>
+     */
+    @Override
+    public List<String> getRoleList(Object loginId, String loginType) {
+        // 本list仅做模拟，实际项目中要根据具体业务逻辑来查询角色
+        List<String> list = new ArrayList<>();
+        list.add("admin");
+        list.add("super-admin");
+        return list;
+    }
+}
