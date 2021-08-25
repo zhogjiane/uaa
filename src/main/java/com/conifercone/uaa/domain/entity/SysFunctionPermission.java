1c13ae68250dbb4d06b8bdc120dd2096f73fd80b
单开宇 <547913250@qq.com>
2021-08-25 11:51:44 +0800

    增加功能权限实体和数据库交互文件

---
+++ src/main/java/com/conifercone/uaa/domain/entity/SysFunctionPermission.java
@@ -0,0 +1,28 @@
+package com.conifercone.uaa.domain.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * 系统功能权限
+ *
+ * @author sky5486560@gmail.com
+ * @date 2021/8/25
+ */
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = true)
+@TableName("sys_function_permission")
+@ApiModel(value = "系统功能权限")
+public class SysFunctionPermission extends BaseEntity {
+
+    @ApiModelProperty(value = "权限编码")
+    private String permissionCode;
+
+    @ApiModelProperty(value = "权限名称")
+    private String permissionName;
+}
