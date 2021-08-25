a4610e2bb659a2c2377b29e8bcd043d1f7e44a83
单开宇 <547913250@qq.com>
2021-08-25 11:19:57 +0800

    更新数据库初始化sql

--- database/sql/init.sql
+++ database/sql/init.sql
@@ -34,7 +34,7 @@
  Target Server Version : 50732
  File Encoding         : 65001
 
- Date: 17/08/2021 17:23:08
+ Date: 25/08/2021 11:18:29
 */
 
 SET NAMES utf8mb4;
@@ -44,40 +44,115 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for sys_oauth2_client
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_oauth2_client`;
-CREATE TABLE `sys_oauth2_client`  (
-                                      `id` bigint(40) NOT NULL COMMENT '主键id',
-                                      `client_id` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '客户端id',
-                                      `client_secret` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '客户端密钥',
-                                      `allow_url` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '允许访问的地址',
-                                      `contract_scope` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '契约范围',
-                                      `create_by` bigint(40) NULL DEFAULT NULL COMMENT '创建人',
-                                      `create_time` timestamp(0) NULL DEFAULT NULL COMMENT '创建时间',
-                                      `update_by` bigint(40) NULL DEFAULT NULL COMMENT '更新人',
-                                      `update_time` timestamp(0) NULL DEFAULT NULL COMMENT '更新时间',
-                                      `removed` tinyint(4) NULL DEFAULT NULL COMMENT '是否删除',
-                                      PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = 'Oauth2客户端信息表' ROW_FORMAT = Dynamic;
+CREATE TABLE `sys_oauth2_client`
+(
+    `id`             bigint(40)                                              NOT NULL COMMENT '主键id',
+    `client_id`      varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '客户端id',
+    `client_secret`  varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '客户端密钥',
+    `allow_url`      varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '允许访问的地址',
+    `contract_scope` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '契约范围',
+    `create_by`      bigint(40)                                              NULL DEFAULT NULL COMMENT '创建人',
+    `create_time`    timestamp                                               NULL DEFAULT NULL COMMENT '创建时间',
+    `update_by`      bigint(40)                                              NULL DEFAULT NULL COMMENT '更新人',
+    `update_time`    timestamp                                               NULL DEFAULT NULL COMMENT '更新时间',
+    `removed`        tinyint(4)                                              NULL DEFAULT 0 COMMENT '是否删除',
+    `tenant_id`      bigint(40)                                              NULL DEFAULT NULL COMMENT '租户id',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci COMMENT = 'Oauth2客户端信息表'
+  ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`
+(
+    `id`          bigint(40)                                              NOT NULL,
+    `role_code`   varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `role_name`   varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    `create_by`   bigint(40)                                              NULL DEFAULT NULL,
+    `create_time` timestamp                                               NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `update_by`   bigint(40)                                              NULL DEFAULT NULL,
+    `update_time` timestamp                                               NULL DEFAULT '0000-00-00 00:00:00',
+    `removed`     tinyint(4)                                              NULL DEFAULT 0,
+    `tenant_id`   bigint(40)                                              NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_tenant
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_tenant`;
+CREATE TABLE `sys_tenant`
+(
+    `id`             bigint(40)                                              NOT NULL COMMENT '主键id',
+    `tenant_name`    varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租户名称',
+    `tenant_profile` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '租户简介',
+    `create_by`      bigint(40)                                              NULL DEFAULT NULL COMMENT '创建人',
+    `create_time`    timestamp                                               NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_by`      bigint(40)                                              NULL DEFAULT NULL COMMENT '更新人',
+    `update_time`    timestamp                                               NULL DEFAULT NULL COMMENT '更新时间',
+    `removed`        tinyint(4)                                              NULL DEFAULT 0 COMMENT '是否删除',
+    `tenant_id`      bigint(40)                                              NULL DEFAULT NULL COMMENT '租户id'
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci COMMENT = '系统租户表'
+  ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for sys_user
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user`  (
-                             `id` bigint(40) NOT NULL,
-                             `account_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '账户名称',
-                             `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
-                             `real_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '真实名称',
-                             `sex` char(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
-                             `phone_number` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号',
-                             `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
-                             `create_by` bigint(40) NULL DEFAULT NULL COMMENT '创建人',
-                             `create_time` timestamp(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
-                             `update_by` bigint(40) NULL DEFAULT NULL COMMENT '更新人',
-                             `update_time` timestamp(0) NOT NULL COMMENT '更新时间',
-                             `removed` tinyint(4) NULL DEFAULT NULL COMMENT '是否已删除',
-                             PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '系统用户表' ROW_FORMAT = Dynamic;
+CREATE TABLE `sys_user`
+(
+    `id`               bigint(40)                                              NOT NULL,
+    `account_name`     varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '账户名称',
+    `password`         varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+    `real_name`        varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '真实名称',
+    `sex`              char(10) CHARACTER SET utf8 COLLATE utf8_general_ci     NULL DEFAULT NULL COMMENT '性别',
+    `phone_number`     varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL COMMENT '手机号',
+    `email`            varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL COMMENT '邮箱',
+    `create_by`        bigint(40)                                              NULL DEFAULT NULL COMMENT '创建人',
+    `create_time`      timestamp                                               NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_by`        bigint(40)                                              NULL DEFAULT NULL COMMENT '更新人',
+    `update_time`      timestamp                                               NULL DEFAULT NULL COMMENT '更新时间',
+    `removed`          tinyint(4)                                              NULL DEFAULT 0 COMMENT '是否已删除',
+    `tenant_id`        bigint(40)                                              NULL DEFAULT NULL COMMENT '租户id',
+    `data_permissions` int(50)                                                 NULL DEFAULT NULL COMMENT '用户数据权限范围',
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci COMMENT = '系统用户表'
+  ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role`
+(
+    `id`          bigint(40) NOT NULL,
+    `role_id`     bigint(40) NULL DEFAULT NULL,
+    `user_id`     bigint(40) NULL DEFAULT NULL,
+    `create_by`   bigint(40) NULL DEFAULT NULL,
+    `create_time` timestamp  NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `update_by`   bigint(40) NULL DEFAULT NULL,
+    `update_time` timestamp  NULL DEFAULT '0000-00-00 00:00:00',
+    `removed`     tinyint(4) NULL DEFAULT 0,
+    `tenant_id`   bigint(40) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `FK_Reference_1` (`user_id`) USING BTREE,
+    INDEX `FK_Reference_2` (`role_id`) USING BTREE,
+    CONSTRAINT `FK_Reference_1` FOREIGN KEY (`user_id`) REFERENCES `sys_user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    CONSTRAINT `FK_Reference_2` FOREIGN KEY (`role_id`) REFERENCES `sys_role` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
-
-
