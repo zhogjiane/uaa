70ffe979d9c62e4c9e4a59acf1abc6bc60b4758e
单开宇 <547913250@qq.com>
2021-08-25 15:50:33 +0800

    增加功能权限分页查询单元测试

---
+++ src/test/java/com/conifercone/uaa/service/impl/FunctionPermissionServiceImplTest.java
@@ -0,0 +1,65 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) [2021] [sky5486560@gmail.com]
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.conifercone.uaa.service.impl;
+
+import cn.dev33.satoken.session.SaSession;
+import cn.dev33.satoken.stp.StpUtil;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.conifercone.uaa.UaaApplicationServer;
+import com.conifercone.uaa.domain.constant.UserSessionDataNameConstant;
+import com.conifercone.uaa.domain.vo.SysFunctionPermissionVO;
+import com.conifercone.uaa.service.IFunctionPermissionService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+/**
+ * 功能权限业务逻辑层单元测试
+ *
+ * @author sky5486560@gmail.com
+ * @date 2021/8/25
+ */
+@SpringBootTest(classes = UaaApplicationServer.class)
+class FunctionPermissionServiceImplTest {
+
+    @Resource
+    IFunctionPermissionService functionPermissionService;
+
+    /**
+     * 分页查询功能权限
+     */
+    @Test
+    void pagingQueryFunctionPermissions() {
+        StpUtil.login(0);
+        SaSession session = StpUtil.getSession();
+        session.set(UserSessionDataNameConstant.TENANT_ID, 0);
+        session.set(UserSessionDataNameConstant.DATA_PERMISSIONS, 3);
+        IPage<SysFunctionPermissionVO> sysFunctionPermissionVOIPage = functionPermissionService
+                .pagingQueryFunctionPermissions(1, 10, new SysFunctionPermissionVO());
+        Assertions.assertNotNull(sysFunctionPermissionVOIPage);
+    }
+}
