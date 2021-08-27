c5a68b8328bc7dad52ac11c6f1fb7e63ee3cf584
单开宇 <547913250@qq.com>
2021-08-27 17:17:38 +0800

    BaseVO增加序列化

--- src/main/java/com/conifercone/uaa/domain/vo/BaseVO.java
+++ src/main/java/com/conifercone/uaa/domain/vo/BaseVO.java
@@ -29,6 +29,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 /**
@@ -39,7 +40,9 @@ import java.time.LocalDateTime;
  */
 @Data
 @ApiModel(value = "基础值对象")
-public class BaseVO {
+public class BaseVO implements Serializable {
+
+    private static final long serialVersionUID = 1579510534898095477L;
 
     @ApiModelProperty(value = "主键id")
     protected Long id;
